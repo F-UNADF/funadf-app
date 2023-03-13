5e3546407330372130c320b797536a802a4c1bd2
Paul Gruson <p.gruson@gmail.com>
2023-03-13 19:06:22 +0100

    Manage Fees

--- src/components/Users/card.vue
+++ src/components/Users/card.vue
@@ -2,6 +2,16 @@
     <ion-card color="primary">
         <ion-card-content>
             <qrcode-vue :value="user.passphrase" size="300" render-as="svg" class="qrcode"></qrcode-vue>
+
+            <ion-row class="ion-justify-content-center">
+                <ion-col v-for="(year, index) in years" :key="index">
+                    <ion-chip :color="getChipColor(year)">
+                        <i class="material-icons">{{ getChipIcon(year) }}</i>
+                        {{ year }}
+                    </ion-chip>
+                </ion-col>
+            </ion-row>
+
         </ion-card-content>
         <ion-card-header style="text-align: center;">
             <ion-card-title>{{ user.fullname }}</ion-card-title>
@@ -29,10 +39,34 @@ export default {
     computed: {
         ...mapGetters({
             user: 'getUser',
+            fees: 'getFees',
         }),
+    },
+    methods: {
+        hasFeeForYear(year) {
+            console.log(year);
+
+            return this.fees.filter(fee => fee.what === year.toString()).length > 0;
+        },
+        getChipColor(year) {
+            return this.hasFeeForYear(year) ? 'success' : 'danger';
+        },
+        getChipIcon(year) {
+            return this.hasFeeForYear(year) ? 'done' : 'close';
+        },
     },
     beforeCreate: function () {
         this.$store.dispatch('getConnectedUser');
+    },
+    data() {
+        let years = [];
+        let currentYear = new Date().getFullYear();
+        for (let i = 0; i < 6; i++) {
+            years.push(currentYear - i);
+        }
+        return {
+            years: years,
+        }
     },
 };
 </script>
